3cb80cb219ba857eb0f86bdfd7e20ce22f531b5e
akhileshkr797 <akhileshkr797@gmail.com>
2019-04-29 13:14:13 +0530

    removed minimized/maximized functionality

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 const electron = require('electron')
-const {app, BrowserWindow, webContents} = electron
+const {app, BrowserWindow, webContents, MenuItem, ipcMain, Menu} = electron
 const path = require('path')
 const url = require('url')
 
 
 let mainWindow = null
 
-app.on('ready', ()=>{
-    console.log('hello from electron')
-    mainWindow = new BrowserWindow()
-    mainWindow.loadFile('index.html')
+function createWindow(){
+    mainWindow = new BrowserWindow({
+    minimizable: false,
+    maximizable: false,
+    width:800,
+    height:650,
+    title:'mainWindow'
+    })
+
+    mainWindow.loadURL(url.format({
+        pathname:path.join(__dirname, 'index.html'),
+        protocol:'file:',
+        slashes:true
+    }))
     
-    //devTool
-    //mainWindow.webContents.openDevTools()
+    mainWindow.on('close', function(){
+        mainWindow = null
+    })
+}
+
+app.on('ready', ()=>{
+    createWindow()
 })
